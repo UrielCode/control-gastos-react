96489e0cd8dcfd1c8490f01533ad93ead8a951bf
Uriel Rdguez <uriel.rdguez@gmail.com>
2023-06-28 08:48:04 -0600

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import NewExpense from "./components/NewExpensive/NewExpense";
 const DUMMY_EXPENSES = [
   {
     id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
+    title: 'Veterinaria',
+    amount: 994,
     date: new Date(2022, 7, 14),
   },
   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
     id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
+    title: 'Seguro Medico',
+    amount: 1494.67,
     date: new Date(2021, 2, 28),
   },
   {
     id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
+    title: 'Comida',
+    amount: 850,
     date: new Date(2023, 5, 12),
   },
 ];
